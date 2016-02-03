4e0dc44929831c503554cc90e6f62cde88eb32bd
Mike Wyatt <wyatt.mike@gmail.com>
2016-02-02 23:44:28 -0400

    add shouldFailOnWarning to parched-eslint

--- parched-eslint/index.js
+++ parched-eslint/index.js
@@ -1,12 +1,36 @@
 var eslint = require('gulp-eslint')
 
 module.exports = function (Parched) {
+  // Seems the maintainers really don't want "treat warnings as errors"
+  // functionality
+  // https://github.com/eslint/eslint/issues/2309
+  function failAfterWarning () {
+    return eslint.results(function(results) {
+      var count = results.warningCount
+      if (count > 0) {
+        throw new Parched.vendor.gutil.PluginError(
+          'gulp-eslint',
+          {
+            name: 'ESLintError',
+            message: 'Failed with ' + count + (count === 1 ? ' warning' : ' warnings')
+          }
+        )
+      }
+    })
+  }
+
   Parched.createPlugin({
     displayName: 'parched-eslint',
     src: [
       '*.js',
       '*.jsx',
     ],
+
+    getDefaultOptions: function () {
+      return {
+        shouldFailOnWarning: false,
+      }
+    },
 
     lint: function () {
       var stream = [
@@ -16,6 +40,9 @@ module.exports = function (Parched) {
 
       if (this.isProduction()) {
         stream.push(eslint.failAfterError())
+        if (this.options.shouldFailOnWarning) {
+          stream.push(failAfterWarning())
+        }
       }
 
       return stream

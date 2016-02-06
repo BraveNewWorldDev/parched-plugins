e1fc497e5e843674b386370506c75e826b9deb18
Mike Wyatt <wyatt.mike@gmail.com>
2016-02-06 16:17:51 -0400

    default add prefixes with autoprefixer in parched-cssnano

--- parched-cssnano/index.js
+++ parched-cssnano/index.js
@@ -4,6 +4,13 @@ module.exports = function(Parched) {
   return Parched.createPlugin({
     displayName: 'parched-cssnano',
     src: '*.css',
+    getDefaultOptions: function () {
+      return {
+        autoprefixer: {
+          add: true,
+        },
+      }
+    },
     minify: function() {
       return cssnano(this.options);
     }
